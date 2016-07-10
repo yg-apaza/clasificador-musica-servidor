e6a5bcf40216f84d3a742362955510eaa9e11439
Yuliana Apaza <yuliana.apaza94@gmail.com>
2016-07-09 20:20:10 -0500

    Testing env variables

--- app/dbconnect.py
+++ app/dbconnect.py
@@ -6,7 +6,6 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_USER', '')
-
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', '')
 app.config['MYSQL_DATABASE_DB'] = 'clasificador'
 app.config['MYSQL_DATABASE_HOST'] = os.environ.get('OPENSHIFT_MYSQL_DB_HOST',
@@ -14,6 +13,9 @@ app.config['MYSQL_DATABASE_HOST'] = os.environ.get('OPENSHIFT_MYSQL_DB_HOST',
 app.config['MYSQL_DATABASE_PORT'] = int(os.environ.get(
                                                     'OPENSHIFT_MYSQL_DB_PORT',
                                                     ''))
+
+print "---------------------------------here--------------------------------"
+print os.environ.get('MYSQL_PASSWORD', '')
 
 mysql.init_app(app)
 
