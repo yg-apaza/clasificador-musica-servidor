04710ad7c3f9dd1165a1b1446ae66e0efd71e04a
Yuliana Apaza <yuliana.apaza94@gmail.com>
2016-07-14 12:49:42 -0500

    Demo NEAT

--- app/common.py
+++ app/common.py
@@ -1,5 +1,6 @@
 import json
 import os
+import numpy as np
 
 
 def load(var):
@@ -24,3 +25,44 @@ def loadDict(filename):
     with open(filename) as data_file:
         data = json.load(data_file)
     return data
+
+
+def featureDictToArray(dict):
+    arr = np.empty(len(dict))
+    arr[0] = dict['mean-mean-Centroid']
+    arr[1] = dict['mean-std-Centroid']
+    arr[2] = dict['std-mean-Centroid']
+    arr[3] = dict['std-std-Centroid']
+    arr[4] = dict['mean-mean-RollOff']
+    arr[5] = dict['mean-std-RollOff']
+    arr[6] = dict['std-mean-RollOff']
+    arr[7] = dict['std-std-RollOff']
+    arr[8] = dict['mean-mean-Flux']
+    arr[9] = dict['mean-std-Flux']
+    arr[10] = dict['std-mean-Flux']
+    arr[11] = dict['std-std-Flux']
+    arr[12] = dict['mean-mean-ZeroCrossings']
+    arr[13] = dict['mean-std-ZeroCrossings']
+    arr[14] = dict['std-mean-ZeroCrossings']
+    arr[15] = dict['std-std-ZeroCrossings']
+    arr[16] = dict['mean-mean-coeficiente1']
+    arr[17] = dict['mean-std-coeficiente1']
+    arr[18] = dict['std-mean-coeficiente1']
+    arr[19] = dict['std-std-coeficiente1']
+    arr[20] = dict['mean-mean-coeficiente2']
+    arr[21] = dict['mean-std-coeficiente2']
+    arr[22] = dict['std-mean-coeficiente2']
+    arr[23] = dict['std-std-coeficiente2']
+    arr[24] = dict['mean-mean-coeficiente3']
+    arr[25] = dict['mean-std-coeficiente3']
+    arr[26] = dict['std-mean-coeficiente3']
+    arr[27] = dict['std-std-coeficiente3']
+    arr[28] = dict['mean-mean-coeficiente4']
+    arr[29] = dict['mean-std-coeficiente4']
+    arr[30] = dict['std-mean-coeficiente4']
+    arr[31] = dict['std-std-coeficiente4']
+    arr[32] = dict['mean-mean-coeficiente5']
+    arr[33] = dict['mean-std-coeficiente5']
+    arr[34] = dict['std-mean-coeficiente5']
+    arr[35] = dict['std-std-coeficiente5']
+    return arr
---
+++ app/train/NEAT.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+
+import os
+from neat import nn, population, statistics
+from app.dbconnect import conn
+from app import common
+import numpy as np
+# Network inputs and expected outputs.
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM songs")
+rv = cur.fetchall()
+
+inputsTrain = np.empty([len(rv), 36])
+outputsTrain = np.empty([len(rv), 4])
+
+
+for i in range(0, len(rv)):
+    dict = common.loadDict(os.path.join(common.load('data_dir'),
+                           rv[i]['data']))
+    arr = common.featureDictToArray(dict)
+    print(arr.shape)
+    inputsTrain[i, :] = arr
+    temp = np.zeros(4)
+    temp[int(rv[i]['genre']) - 1] = 1
+    outputsTrain[i, :] = temp
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print(inputsTrain)
+print(outputsTrain)
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+
+def eval_fitness(genomes):
+    for g in genomes:
+        net = nn.create_feed_forward_phenotype(g)
+        print(g)
+        sum_square_error = 0.0
+        i = 0
+        for inputs, expected in zip(inputsTrain, outputsTrain):
+            # Serial activation propagates
+            # the inputs through the entire network.
+            output = net.serial_activate(inputs)
+            if(i == 0):
+                # print(output)
+                # print(expected)
+                print((output - expected) ** 2)
+            sum_square_error += np.sum((output - expected) ** 2)
+            i += 1
+        # When the output matches expected for all inputs, fitness will reach
+        # its maximum value of 1.0.
+        g.fitness = 1 - sum_square_error
+
+
+local_dir = os.path.dirname(__file__)
+config_path = os.path.join(local_dir, 'neuron.config')
+pop = population.Population(config_path)
+pop.run(eval_fitness, 300)
+
+# Log statistics.
+statistics.save_stats(pop.statistics)
+statistics.save_species_count(pop.statistics)
+statistics.save_species_fitness(pop.statistics)
+
+print('Number of evaluations: {0}'.format(pop.total_evaluations))
+
+# Show output of the most fit genome against training data.
+winner = pop.statistics.best_genome()
+print('\nBest genome:\n{!s}'.format(winner))
+print('\nOutput:')
+winner_net = nn.create_feed_forward_phenotype(winner)
+for inputs, expected in zip(inputsTrain, outputsTrain):
+    output = winner_net.serial_activate(inputs)
+    print("expected {0:1.5f} got {1:1.5f}".format(expected, output[0]))
