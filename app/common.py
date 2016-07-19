18d7ff91ff143a90fe27e90e5c050eceb2d864b1
Yuliana Apaza <yuliana.apaza94@gmail.com>
2016-07-19 05:42:44 -0500

    Openhift

--- app/common.py
+++ app/common.py
@@ -6,7 +6,7 @@ import numpy as np
 def load(var):
     # with open('settingsOpenshift.json') as data_file:
     with open(os.path.join(os.path.dirname(__file__),
-                           'settings.json')) as data_file:
+                           'settingsOpenshift.json')) as data_file:
         data = json.load(data_file)
     if data[var]['type'] == 'env':
         return os.environ.get(data[var]['value'], '')
